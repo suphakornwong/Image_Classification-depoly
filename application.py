6a53b661ab812233735b46e3bdf5fa2d7691ff31
suphakornwong <77874904+suphakornwong@users.noreply.github.com>
2026-08-18 16:11:16 +0700

    Update application.py

--- application.py
+++ application.py
@@ -10,16 +10,17 @@ def main():
     st.write('<p style="font-size: 22px;">ผลการจัดจำแนกรูปภาพ Image classify from YOLO Model</p>', unsafe_allow_html=True)
     st.sidebar.header("งานวิจัยของศุภกร วงษ์เรืองพิบูล")
 
-    model_path = "BestImageClassify.pt"
-    uploaded_file = st.sidebar.file_uploader("Please selected image file", type=["jpg", "jpeg", "png"])
+    model_path = "bestAcc1=0.842.pt"
+    uploaded_file = st.sidebar.file_uploader("Please select image file…", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        tfile = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg")
-        tfile.write(uploaded_file.read())
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tfile:
+            tfile.write(uploaded_file.read())
+            tmp_path = tfile.name
         model = YOLO(model_path)
-        img = cv2.imread(tfile.name)
+        img = cv2.imread(tmp_path)
         st.sidebar.info("Image Classifying ...")
-        results = model(tfile.name, conf=0.05, imgsz=640)[0]
+        results = model(tfile.name, imgsz=640)[0]
         
         if results.probs is not None and hasattr(results, 'names'):
             predicted_class_index = int(results.probs.data.argmax())
@@ -28,14 +29,15 @@ def main():
         else:
             predicted_class = 'Unknown'
             confidence = 0.0
-        
+
         img_with_boxes = results.plot()
         img_rgb = cv2.cvtColor(img_with_boxes, cv2.COLOR_BGR2RGB)
         st.image(img_rgb, width=640, caption=f'ผลการจัดจำแนกอันดับที่ 1: {predicted_class} (ความมั่นใจ {confidence:.2f}%)')
+
         try:
             tfile.close()
             os.unlink(tfile.name)
         except Exception as e:
             st.error(f" An error occurred while deleting temporary files: {e}")
-main()
 
+main()
